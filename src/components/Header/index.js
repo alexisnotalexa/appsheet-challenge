127ca1e846b8df5a6fe57d3fc85ed7f60c0972f3
Alexis <alexiskokamura@gmail.com>
2018-04-10 17:15:04 -1000

    added some styles to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,10 @@ import './styles.css';
 const Header = () => {
   return (
     <header>
-      <h1>Header</h1>
+      <span class="fa-layers fa-fw">
+        <i class="fas fa-circle fa-3x"></i>
+        <i class="fa-inverse fab fa-telegram-plane fa-4x" data-fa-transform="rotate-30 left-3 up-1"></i>
+      </span>
     </header>
   );
 }
