a2e154acb54f6e1a43423dfdd1a5fad14f43642f
Alexis <alexiskokamura@gmail.com>
2018-04-10 16:32:32 -1000

    added filter for valid us phone numbers

---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const Card = () => {
+  return (
+    <div>
+      <h3>Card</h3>
+    </div>
+  );
+}
+
+export default Card;
--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './styles.css';
 
+// COMPONENTS
+import Card from '../../components/Card';
+
 // UTILS
 import { getAllIds } from '../../utils/users.js';
 
@@ -9,8 +12,13 @@ class Main extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      usersLoaded: false
+    };
+
     // functions
     this.getUserData = this.getUserData.bind(this);
+    this.filterUsers = this.filterUsers.bind(this);
   }
 
   componentDidMount() {
@@ -31,18 +39,45 @@ class Main extends Component {
       }).then(results => {
         return results.map(user => {
           return user ? user.data : false;
-        });
+        }).filter(user => user);
       }).then(users => {
-        console.log(users);
+        this.setState({
+          usersLoaded: users
+        });
+        this.filterUsers();
       });
   }
 
+  filterUsers() {
+    let regex = /^(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/g;
+    let sorted = this.state.usersLoaded
+      .sort((a, b) => {
+        if(a.age < b.age) return -1;
+        if(a.age > b.age) return 1;
+        return 0;
+      })
+      .filter(user => user.number && regex.test(user.number))
+      .map((user, index) => {
+        return index < 5 ? user : false;
+      })
+      .filter(user => user);
+    console.log(sorted);
+  }
+
   render() {
-    return (
-      <div>
-        <h1>Main</h1>
-      </div>
-    );
+    // let cards = this.state.usersLoaded &&
+    //   this.state.usersLoaded.map(user => {
+    //     console.log(user);
+    //   });
+    if(this.state.usersLoaded) {
+      return (
+        <div>
+          <h1>Main</h1>
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
 
