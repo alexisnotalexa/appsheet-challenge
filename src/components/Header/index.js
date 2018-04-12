f2a24da9eaba261b401d8bf43d5a049c081eb5f4
Alexis <alexiskokamura@gmail.com>
2018-04-11 14:58:38 -1000

    added more styles to cards

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,8 @@ const Header = () => {
   return (
     <header>
       <span class="fa-layers fa-fw">
-        <i class="fas fa-circle fa-3x"></i>
-        <i class="fa-inverse fab fa-telegram-plane fa-4x" data-fa-transform="rotate-30 left-3 up-1"></i>
+        <i class="fas fa-circle fa-4x"></i>
+        <i class="fa-inverse fab fa-telegram-plane fa-3x" data-fa-transform="rotate-30"></i>
       </span>
     </header>
   );
