83259d84582e73bb3767c3e0b5113f97387d3584
Alexis <alexiskokamura@gmail.com>
2018-04-12 09:11:59 -1000

    working on header style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,10 +4,10 @@ import './styles.css';
 const Header = () => {
   return (
     <header>
-      <span class="fa-layers fa-fw fa-4x">
-        <i class="fas fa-circle"></i>
-        <i class="fa-inverse fab fa-telegram-plane" data-fa-transform="shrink-6 down-.5 left-.5"></i>
-      </span>
+      <div className="header__bg" />
+      <div className="header__circle">
+        <i class="fa-inverse fab fa-telegram-plane fa-2x"></i>
+      </div>
     </header>
   );
 }
